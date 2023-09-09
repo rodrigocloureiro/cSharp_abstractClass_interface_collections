317c18268ed599c613299872fb616306e49523bb
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 21:29:28 -0300

    listas de contas e seguros criadas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿public class Program
+﻿using exe02lista;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-
+        IList<Conta> contas = new List<Conta>();
+        IList<SeguroDeVida> seguros = new List<SeguroDeVida>();
     }
 }
