8197353d311aa978e3fd850c45221eab50b63d75
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 21:17:21 -0300

    classe SeguroDeVida criada e codificada

---
+++ SeguroDeVida.cs
@@ -0,0 +1,12 @@
+﻿namespace exe02lista
+{
+    public class SeguroDeVida : ITributavel
+    {
+        private const double _TAXA = 42.00;
+
+        public double CalculaTributos()
+        {
+            return _TAXA;
+        }
+    }
+}
