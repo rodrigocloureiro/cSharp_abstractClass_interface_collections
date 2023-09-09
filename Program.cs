d633995e558fb18027a3684e774abe15a91c3e44
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 21:45:01 -0300

    listas populadas

--- Program.cs
+++ Program.cs
@@ -6,5 +6,12 @@ public class Program
     {
         IList<Conta> contas = new List<Conta>();
         IList<SeguroDeVida> seguros = new List<SeguroDeVida>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            contas.Add(new ContaCorrente(Math.Floor((double)new Random().Next() * 3000 + 2000), $"Nome{Math.Floor((double)new Random().Next() * 100)}"));
+            contas.Add(new ContaPoupanca(Math.Floor((double)new Random().Next() * 3000 + 2000), $"Nome{Math.Floor((double)new Random().Next() * 100)}"));
+            seguros.Add(new SeguroDeVida());
+        }
     }
 }
