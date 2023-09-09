870a7dde145e883c570308dcbb22ce34166d827a
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 21:25:17 -0300

    class ContaCorrente criada e codificada

---
+++ ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿namespace exe02lista
+{
+    public class ContaCorrente : Conta, ITributavel
+    {
+        public ContaCorrente(double saldo, string nome) : base(saldo, nome) { }
+
+        public double CalculaTributos()
+        {
+            return ObterSaldo() * 0.01;
+        }
+    }
+}
